1ccb71d15d3d49f473474f3c2fda39a132e9678d
Max Beatty <max@beatty.me>
2020-08-23 12:13:03 -0700

    expose internals for secondary tabs
    current API works great for single set of tabs
    if you need a secondary set for like mobile, need more access

--- src/index.js
+++ src/index.js
@@ -4,76 +4,84 @@ import React, {
   useMemo,
   useContext,
   cloneElement,
-  isValidElement
-} from 'react'
+  isValidElement,
+} from "react";
 
-import useConstant from 'use-constant'
+import useConstant from "use-constant";
 
-const TabsState = createContext()
-const Elements = createContext()
+const TabsState = createContext();
+const Elements = createContext();
 
 export const Tabs = ({ state: outerState, children }) => {
-  const innerState = useState(0)
-  const elements = useConstant(() => ({ tabs: 0, panels: 0 }))
-  const state = outerState || innerState
+  const innerState = useState(0);
+  const elements = useConstant(() => ({ tabs: 0, panels: 0 }));
+  const state = outerState || innerState;
 
   return (
     <Elements.Provider value={elements}>
       <TabsState.Provider value={state}>{children}</TabsState.Provider>
     </Elements.Provider>
-  )
-}
+  );
+};
 
 export const useTabState = () => {
-  const [activeIndex, setActive] = useContext(TabsState)
-  const elements = useContext(Elements)
+  const [activeIndex, setActive] = useContext(TabsState);
+  const elements = useContext(Elements);
 
   const tabIndex = useConstant(() => {
-    const currentIndex = elements.tabs
-    elements.tabs += 1
+    const currentIndex = elements.tabs;
+    elements.tabs += 1;
 
-    return currentIndex
-  })
+    return currentIndex;
+  });
 
-  const onClick = useConstant(() => () => setActive(tabIndex))
+  const onClick = useConstant(() => () => setActive(tabIndex));
 
   const state = useMemo(
     () => ({
       isActive: activeIndex === tabIndex,
-      onClick
+      onClick,
+      activeIndex,
+      setActive,
     }),
     [activeIndex, onClick, tabIndex]
-  )
+  );
 
-  return state
-}
+  return state;
+};
+
+export const useUnsafeTabState = () => {
+  const [activeIndex, setActive] = useContext(TabsState);
+
+  return { activeIndex, setActive };
+};
 
 export const usePanelState = () => {
-  const [activeIndex] = useContext(TabsState)
-  const elements = useContext(Elements)
+  const [activeIndex] = useContext(TabsState);
+  const elements = useContext(Elements);
 
   const panelIndex = useConstant(() => {
-    const currentIndex = elements.panels
-    elements.panels += 1
+    const currentIndex = elements.panels;
+    elements.panels += 1;
 
-    return currentIndex
-  })
+    return currentIndex;
+  });
 
-  return panelIndex === activeIndex
-}
+  return panelIndex === activeIndex;
+};
 
 export const Tab = ({ children }) => {
-  const state = useTabState()
+  const state = useTabState();
 
-  if (typeof children === 'function') {
-    return children(state)
+  if (typeof children === "function") {
+    return children(state);
   }
 
-  return isValidElement(children) ? cloneElement(children, state) : children
-}
+  return isValidElement(children) ? cloneElement(children, state) : children;
+};
 
 export const Panel = ({ active, children }) => {
-  const isActive = usePanelState()
+  const isActive = usePanelState();
 
-  return isActive || active ? children : null
-}
+  return isActive || active ? children : null;
+};
